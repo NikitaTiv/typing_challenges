be24d2aeb7b63bf312d4614b380ca0952a133d42
NikitaTiv <118524352+NikitaTiv@users.noreply.github.com>
2023-02-27 05:12:27 +0300

    Add lvl 3

--- level_3/3.py
+++ level_3/3.py
@@ -1,13 +1,13 @@
 from constants import ___
+from typing import Any
 
 
-def create_user(user_name: ___, user_age: ___, after_created: ___) -> ___:
+def create_user(user_name: str, user_age: int, after_created: Any) -> None:
     pass
 
 
 def send_test_email(user_id: int) -> None:
     pass
-
 
 if __name__ == "__main__":
     assert create_user(
